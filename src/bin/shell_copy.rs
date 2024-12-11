8799cd962711286f644716ac101c77d78440276f
pzy <1697437561@qq.com>
2024-12-11 17:33:53 +0800

    U盘盘符名和脚本路径可配置

--- src/bin/shell_copy.rs
+++ src/bin/shell_copy.rs
@@ -1,8 +1,9 @@
-use std::{thread::sleep, time::Duration};
+use std::{fs::File, io::{BufRead, BufReader}, thread::sleep, time::Duration};
 
 use hidg::{Class, Device, Key, Keyboard};
+use log::info;
 use simple_logger::SimpleLogger;
-use hid_simulator::KeyboardHelper;
+use hid_simulator::{KeyboardHelper, CONFIG_FILE_PATH};
 
 // const DRIVER_NAME: &str = "MYUDISK";
 // const SCRIPT_PATH: &str = "hideAndRun.ps1";
@@ -13,6 +14,13 @@ fn main() -> std::io::Result<()> {
     let mut device = Device::<Keyboard>::open(0)?;
     let mut input = Keyboard.input();
     let mut key_helper = KeyboardHelper::new(&mut device, &mut input);
+
+    let config_file = File::open(CONFIG_FILE_PATH)?;
+    let reader = BufReader::new(config_file);
+    let mut config_contents = reader.lines();
+    let driver_letter = config_contents.next().expect("failed to resolve driver letter from keyboard config file")?;
+    let script_path = config_contents.next().expect("failed to resolve script path from keyboard config file")?;
+
     // let mut simulator = KeySimulator::new(&mut key_helper);
 
     // simulator.open_powershell_admin()?;
@@ -21,11 +29,17 @@ fn main() -> std::io::Result<()> {
     //     -ArgumentList \"-ExecutionPolicy Bypass -WindowStyle Hidden -File ${{{DRIVER_LETTER_VAR_NAME}}}\\{SCRIPT_PATH}\"").as_str())?;
     // simulator.run_command_in_powershell("exit")?;
 
+    info!("HID simulator: driver letter: {}, script path: {}", driver_letter, script_path);
+    
+    let cmd = format!("powershell -ExecutionPolicy Bypass -command \"$d = \
+    (Get-WmiObject -Query 'SELECT DeviceID FROM Win32_LogicalDisk WHERE VolumeName=\\\"{driver_letter}\\\"').DeviceID; \
+    Start-Process -WindowStyle Hidden -FilePath \\\"powershell\\\" -ArgumentList \\\"-File ${{d}}\\{script_path}\\\"\"");
+    
+    info!("HID simulator: cmd: {}", cmd);
+
     key_helper.press_multi(&[Key::LeftMeta, Key::R])?;
     sleep(Duration::from_millis(500));
-    key_helper.press_cmd("powershell -ExecutionPolicy Bypass -command \"$d = \
-        (Get-WmiObject -Query 'SELECT DeviceID FROM Win32_LogicalDisk WHERE VolumeName=\\\"MYUDISK\\\"').DeviceID; \
-        Start-Process -WindowStyle Hidden -FilePath \\\"powershell\\\" -ArgumentList \\\"-File ${d}\\r.ps1\\\"\"")?;
+    key_helper.press_cmd(&cmd)?;
     key_helper.press_one(Key::Enter)?;  // 目前不用管理员启动，管理员则是ctrl + shift + enter
 
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub use keyboard_helper::KeyboardHelper;
 pub use simulator::KeySimulator;
 
 pub const DRIVER_LETTER_VAR_NAME: &str = "driveLetter";
+pub const CONFIG_FILE_PATH: &str = "/etc/hid_simulator.conf";
